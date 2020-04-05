de9b0aa44a79b3fa080436c6c4f34d895fba032e
Junhyung Ki <kijoonh91@gmail.com>
2020-04-05 19:10:25 +0200

    troubleshoot logger func added

--- src/mqtt_pub_sub_example.py
+++ src/mqtt_pub_sub_example.py
@@ -7,19 +7,34 @@ import time
 
 ####################################################################
 def on_message(client, userdata, message):
+    """Callback function, which basically just prints the received messages"""
     print("message received ", str(message.payload.decode("utf-8")))
     print("message topic =", message.topic)
     print("message qos =", message.qos)
     print("message retain flag =", message.retain)
+
+def on_log(client, userdata, level, buf):
+    """
+    Troubleshoot function to procee the logging callback.
+    It simply prints the log message.
+    """
+    print("log: ",buf)
+
+
+
 ####################################################################
+
+
+
 
 broker_address = "test.mosquitto.org"
 
 print("# 1. Creating new instance.")
 client = mqtt.Client("P1")
 
-print("# 2. Attaching function to callback")
+print("# 2. Attaching a message function & a toubleshoot function to callback")
 client.on_message = on_message # without parentheses
+client.on_log = on_log # without parentheses
 
 print("# 3. Connecting to broker")
 client.connect(broker_address)
