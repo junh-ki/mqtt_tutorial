1550f1d0539e5b66956e5d88b90d602b0abf5692
Junhyung Ki <kijoonh91@gmail.com>
2020-04-05 18:58:30 +0200

    the completed example script

--- src/mqtt_pub_sub_example.py
+++ src/mqtt_pub_sub_example.py
@@ -1,15 +1,38 @@
+# This tutorial is done with python version 3.6.8 on Windows 10
+# On Command Prompt, install the mqtt client first with (pip install paho-mqtt)
+
 import paho.mqtt.client as mqtt
 # broker_address = "iot.eclipse.org" # doesn't work at the moment somehow
+import time
+
+####################################################################
+def on_message(client, userdata, message):
+    print("message received ", str(message.payload.decode("utf-8")))
+    print("message topic =", message.topic)
+    print("message qos =", message.qos)
+    print("message retain flag =", message.retain)
+####################################################################
+
 broker_address = "test.mosquitto.org"
 
 print("# 1. Creating new instance.")
 client = mqtt.Client("P1")
 
-print("# 2. Connecting to broker")
+print("# 2. Attaching function to callback")
+client.on_message = on_message # without parentheses
+
+print("# 3. Connecting to broker")
 client.connect(broker_address)
 
-print("# 3. Subscribe to topic", "house/bulbs/bulb1")
+print("# 4. Starting the loop")
+client.loop_start()
+
+print("# 5. Subscribe to topic", "house/bulbs/bulb1")
 client.subscribe("house/bulbs/bulb1")
 
-print("# 4. Publish message to topic", "house/bulbs/bulb1")
+print("# 6. Publish message to topic", "house/bulbs/bulb1")
 client.publish("house/bulbs/bulb1", "OFF")
+
+print("# 7. Wait & Stop the loop")
+time.sleep(4)
+client.loop_stop()
